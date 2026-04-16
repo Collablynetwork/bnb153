21dfd0468dc2a0fe25ecbb051922cc8118156db5
Collably Network <165827418+Collablynetwork@users.noreply.github.com>
2026-04-16 23:10:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -82,7 +82,7 @@ module.exports = {
   strategiesIndexPath: path.join(strategiesDir, "index.json"),
 
   supportedKlineIntervals: [
-    "1m", "3m", "5m", "15m", "30m",
+    "1m", "5m", "15m", "30m",
     "1h", "2h", "4h", "6h", "8h", "12h",
     "1d", "3d", "1w",
   ],
@@ -90,8 +90,7 @@ module.exports = {
   supportedFlowPeriods: ["5m", "15m", "30m", "1h", "2h", "4h", "6h", "12h", "1d"],
 
   timeframeHierarchyMap: {
-    "1m": ["1m", "3m", "5m", "15m"],
-    "3m": ["3m", "5m", "15m", "30m"],
+    "1m": ["1m", "5m", "15m"],
     "5m": ["5m", "15m", "30m", "1h"],
     "15m": ["15m", "30m", "1h", "4h"],
     "30m": ["30m", "1h", "2h", "4h"],
